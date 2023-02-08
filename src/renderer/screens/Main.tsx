c29b5225567334e7bc364c11a4b7d9528fead1e7
leejooy96 <leejooy96@gmail.com>
2023-02-08 16:19:07 +0900

    fix handle preload data arguments

--- src/renderer/screens/Main.tsx
+++ src/renderer/screens/Main.tsx
@@ -29,7 +29,7 @@ const Main = () => {
 
   useEffect(() => {
     // Get application version from package.json version string (Using IPC communication)
-    window.mainApi.receive('msgReceivedVersion', (version: string) => {
+    window.mainApi.receive('msgReceivedVersion', (event, version: string) => {
       dispatch(setVersion(version));
     });
 
