82d3590d5ed75f73df51f6eaa1a0658bfa065f85
leejooy96 <leejooy96@gmail.com>
2023-03-16 17:21:59 +0900

    change vite.config.ts config

--- vite.config.ts
+++ vite.config.ts
@@ -8,56 +8,61 @@ import { resolve, dirname } from 'path';
 import { rmSync } from 'fs';
 import { builtinModules } from 'module';
 
-rmSync('dist', { recursive: true, force: true });
-
 const isDEV = process.env.NODE_ENV === 'development';
 
-export default defineConfig({
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
-    alias: {
-      '@': resolve(dirname(fileURLToPath(import.meta.url)), 'src'),
+export default defineConfig(() => {
+  rmSync('dist', { recursive: true, force: true });
+
+  return {
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
+      alias: {
+        '@': resolve(dirname(fileURLToPath(import.meta.url)), 'src'),
+      },
     },
-  },
-  base: './',
-  root: resolve('./src/renderer'),
-  publicDir: resolve('./src/renderer/public'),
-  build: {
-    sourcemap: isDEV,
-    minify: !isDEV,
-    assetsDir: '', // See: https://github.com/electron-vite/electron-vite-vue/issues/287
-    outDir: resolve('./dist'),
-  },
-  plugins: [
-    reactPlugin(),
-    // Docs: https://github.com/gxmari007/vite-plugin-eslint
-    eslintPlugin(),
-    // Docs: https://github.com/electron-vite/vite-plugin-electron
-    electronPlugin([
-      {
-        entry: ['src/main/index.ts'],
-        onstart: (options) => {
-          options.startup(['.', '--no-sandbox']);
-        },
-        vite: {
-          build: {
-            assetsDir: '.',
-            outDir: 'dist/main',
-            rollupOptions: {
-              external: ['electron', ...builtinModules],
+    base: './',
+    root: resolve('./src/renderer'),
+    publicDir: resolve('./src/renderer/public'),
+    build: {
+      sourcemap: isDEV,
+      minify: !isDEV,
+      assetsDir: '', // See: https://github.com/electron-vite/electron-vite-vue/issues/287
+      outDir: resolve('./dist'),
+    },
+    plugins: [
+      reactPlugin(),
+      // Docs: https://github.com/gxmari007/vite-plugin-eslint
+      eslintPlugin(),
+      // Docs: https://github.com/electron-vite/vite-plugin-electron
+      electronPlugin([
+        {
+          entry: ['src/main/index.ts'],
+          onstart: (options) => {
+            options.startup();
+          },
+          vite: {
+            build: {
+              assetsDir: '.',
+              outDir: 'dist/main',
+              rollupOptions: {
+                external: ['electron', ...builtinModules],
+              },
             },
           },
         },
-      },
-      {
-        entry: ['src/preload/index.ts'],
-        vite: {
-          build: {
-            outDir: 'dist/preload',
+        {
+          entry: ['src/preload/index.ts'],
+          onstart: (options) => {
+            options.reload();
+          },
+          vite: {
+            build: {
+              outDir: 'dist/preload',
+            },
           },
         },
-      },
-    ]),
-    rendererPlugin(),
-  ],
+      ]),
+      rendererPlugin(),
+    ],
+  };
 });
