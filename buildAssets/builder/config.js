941d247b5d7d3bf609c59e75f76415b2db93b6bb
leejooy96 <leejooy96@gmail.com>
2023-10-17 10:38:14 +0900

    move `electron-builder` config and use dynamic configurations

---
+++ buildAssets/builder/config.js
@@ -0,0 +1,118 @@
+/* eslint-disable no-template-curly-in-string */
+const dotenv = require('dotenv')
+
+const baseConfig = {
+  productName: 'Retron',
+  appId: 'com.retron.retron',
+  asar: true,
+  extends: null,
+  compression: 'maximum',
+  artifactName: '${productName} ${version}_${arch}.${ext}',
+  directories: {
+    buildResources: './buildAssets/installer/',
+    output: './release/${version}'
+  },
+  files: ['dist/**/*', '!release/**/*', '!tests/**/*'],
+  mac: {
+    bundleVersion: '1.0',
+    hardenedRuntime: true,
+    gatekeeperAssess: false,
+    notarize: false,
+    icon: 'buildAssets/installer/icon.icns',
+    type: 'distribution',
+    target: [
+      {
+        target: 'dmg',
+        arch: ['x64', 'arm64', 'universal']
+      }
+    ]
+  },
+  dmg: {
+    contents: [
+      {
+        x: 410,
+        y: 150,
+        type: 'link',
+        path: '/Applications'
+      },
+      {
+        x: 130,
+        y: 150,
+        type: 'file'
+      }
+    ],
+    sign: false
+  },
+  win: {
+    icon: './buildAssets/installer/icon.ico',
+    target: [
+      {
+        target: 'appx',
+        arch: 'x64'
+      },
+      {
+        target: 'zip',
+        arch: 'x64'
+      },
+      {
+        target: 'portable',
+        arch: 'x64'
+      },
+      {
+        target: 'nsis',
+        arch: 'x64'
+      }
+    ]
+  },
+  portable: {
+    artifactName: '${productName} ${version}_${arch} Portable.${ext}'
+  },
+  nsis: {
+    oneClick: true
+  },
+  linux: {
+    executableName: 'retron',
+    icon: 'buildAssets/installer',
+    category: 'Utility',
+    target: [
+      {
+        target: 'snap',
+        arch: 'x64'
+      },
+      {
+        target: 'deb',
+        arch: 'x64'
+      },
+      {
+        target: 'rpm',
+        arch: 'x64'
+      }
+    ]
+  }
+}
+
+dotenv.config()
+
+baseConfig.copyright = `ⓒ ${new Date().getFullYear()} $\{author}`
+baseConfig.files = [
+  /* A list of files not to be included in the build. */
+  /*
+    (Required) The files and folders listed below should not be included in the build.
+  */
+  'dist/**/*',
+  '!docs/**/*',
+  '!tests/**/*',
+  '!release/**/*'
+]
+
+// TODO: Notarize for macOS
+baseConfig.mac.identity = null
+/* if (process.env.MAC_NOTARIZE === 'true') {
+  baseConfig.afterSign = './buildAssets/builder/notarize.ts'
+} else {
+  baseConfig.mac.identity = null
+} */
+
+module.exports = {
+  ...baseConfig
+}
