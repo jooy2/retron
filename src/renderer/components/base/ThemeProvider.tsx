d221e22be69f08169d26b29ed7091efba18dd4dc
leejooy96 <leejooy96@gmail.com>
2024-02-08 15:43:13 +0900

    remove custom breakpoints

--- src/renderer/components/base/ThemeProvider.tsx
+++ src/renderer/components/base/ThemeProvider.tsx
@@ -11,15 +11,6 @@ const ThemeProvider = ({ children }) => {
   const muiTheme = useMemo(
     () =>
       createTheme({
-        breakpoints: {
-          values: {
-            xs: 0,
-            sm: 600,
-            md: 960,
-            lg: 1280,
-            xl: 1920,
-          },
-        },
         palette: {
           mode: darkTheme ? 'dark' : 'light',
           background: {
